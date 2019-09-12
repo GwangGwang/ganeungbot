ce84e4b572f9b8dc8beb28374f57c2fcce0d7a0a
Eric Gwangseung Kim <gwangseung@hotmail.com>
2019-09-12 19:54:53 -0400

    Initial commit - Go version

---
+++ pkg/telegram/telegram.go
@@ -0,0 +1,45 @@
+package telegram
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	tgbotapi "gopkg.in/telegram-bot-api.v4"
+)
+
+func init() {
+	fmt.Println("can i see this?")
+	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	// Optional: wait for updates and clear them if you don't want to handle
+	// a large backlog of old messages
+	time.Sleep(time.Millisecond * 500)
+	updates.Clear()
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+
+}
