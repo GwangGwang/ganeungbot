ba5c1849ecff2fb92d0711cdd2ac25de6d16d707
GwangGwang <g28kim@gmail.com>
2019-12-25 23:59:15 -0500

    Get rid of exposed API key

--- pkg/main.go
+++ pkg/main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/GwangGwang/ganeungbot/pkg/geocoding"
-
 )
 
 func main() {
 	fmt.Println("test")
 
-	w, _ := geocoding.New("AIzaSyDmwAG0JTlIcZO0nFo6Tu7mQQwIZuhDiQQ")
-
-	ll, str, _ := w.GetGPS("toronto")
-
-	fmt.Printf("location: %+v", ll)
-	fmt.Printf("formatted: %s", str)
 
 	//	fmt.Printf("results: %+v\n", results)
 	//
